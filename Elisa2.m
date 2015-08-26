8a2a95c6e34d33420e625af3c8c277247b493aa1
Elisa Appolloni <elisaappolloni@gmail.com>
2015-08-26 12:54:44 +0200

    added file

---
+++ Elisa2.m
@@ -0,0 +1,18 @@
+
+clear all; close all; clc;
+
+r = normrnd(1,3,[1 100]);
+plot(r)
+
+
+r = normrnd(1,1,[1 140]);
+plot(r)
+
+
+
+% added another comment
+
+% Marcello's comment from his branch
+
+% aggiungo ciao
+
